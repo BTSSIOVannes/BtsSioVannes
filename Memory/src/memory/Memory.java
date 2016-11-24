8b18139b6be48d6b26bef38cd4a790043680b3ee
Alain <Alain@asus>
2016-11-24 18:01:14 +0100

    gestion de conflit

--- Memory/src/memory/Memory.java
+++ Memory/src/memory/Memory.java
@@ -5,6 +5,12 @@ public class Memory {
 	public static void main(String[] args) {
 		System.out.println("coucou demain");
 
+		System.out.println("coucou ");
+
+		System.out.println("alain est trop zentil");
+
+		System.out.println("c'est cool git!!");
+
 	}
 
 }
