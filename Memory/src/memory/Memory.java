366cf580c9d05703454278656d3e3f815ef9d223
admin <admin@BTSWIN7-01.561557n.ac-rennes.fr>
2016-11-24 18:01:26 +0100

    aa

--- Memory/src/memory/Memory.java
+++ Memory/src/memory/Memory.java
@@ -3,11 +3,19 @@ package memory;
 public class Memory {
 
 	public static void main(String[] args) {
+
+		System.out.println("coucou Alain");
+
+
 		System.out.println("coucou ");
 		System.out.println("coucou mon osi...");
-		
-		
+	
+
+		System.out.println("alain est trop zentil");
+
+		System.out.println("c'est cool git!!");
 
 	}
 
 }
+ 
