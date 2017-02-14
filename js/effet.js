af5b0bcf1d5a301902ce201f53d7b3e6c73d1d51
orée lien <aurelfond@gmail.com>
2017-02-14 11:48:40 +0100

    Add files via upload

---
+++ js/effet.js
@@ -0,0 +1,85 @@
+/*const circle1 = new mojs.Shape({
+  fill:           'none',
+  radius:         25,
+  strokeWidth:    { 50 : 0 },
+  scale:          { 0: 1 },
+  angle:          { 'rand(-35, -70)': 0 },
+  duration:       500,
+  left: 0,        top: 0,
+  easing: 'cubic.out',
+  stroke:         'grey',
+});*/
+
+/*const circle2 = new mojs.Shape({
+  fill:           'none',
+  radius:         25,
+  strokeWidth:    { 50 : 0 },
+  scale:          { 0: 1 },
+  angle:          { 'rand(-35, -70)': 0 },
+  duration:       500,
+  left: 0,        top: 0,
+  easing: 'cubic.out',
+  radius:         { 0 : 15 },
+  strokeWidth:    { 30: 0 },
+  stroke:         'black',
+  delay:          'rand(75, 150)'
+});
+*/
+/*document.addEventListener( 'click', function (e) {
+  
+   circle1
+    .tune({ x: e.pageX, y: e.pageY  })
+    .replay();
+  
+  circle2
+    .tune({ x: e.pageX, y: e.pageY  })
+    .replay();
+  
+});
+const burst = new mojs.Burst({
+  radius:   { 0: 100 },
+  count:    5,
+  left: 0,
+  top: 0,
+  children:{
+    fill:       { 'white' : 'grey' },
+  	shape: 'circle',
+  	duration: 1800,
+  	delay : 3
+
+  }
+});*/
+
+
+
+/*const burstrect = new mojs.Burst({
+  left: 0, top: 0,
+  radius:   { 0: 30 },
+  count: 6,
+  angle:    'rand(0, 360)',
+  children: {
+    shape:        'cross',
+    stroke:       '#FBF8EF',
+    fill:         'none',
+    scale:        1,
+    scaleX:       { 1: 0 },
+    easing:       'cubic.out',
+    duration:     800
+  }
+});
+*/
+
+/*  	$('#menu a').mouseenter(function(e) {
+
+
+  	burst
+    .tune({ x: e.pageX, y: e.pageY  })
+    .replay()
+})
+
+  $('#menu a').mouseenter(function(e) {
+
+	burstrect
+	.tune({ x: e.pageX, y: e.pageY  })
+	.replay()
+})*/
